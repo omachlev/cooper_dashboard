2dffc15730fa40aef72af518da392a0a0ab416ef
elad <eladmazaki@gmail.com>
2016-03-14 16:30:08 +0200

    I dont like when git ignores me

--- src/app/controllers/DataCenterController.js
+++ src/app/controllers/DataCenterController.js
@@ -1,27 +1,69 @@
-(function(){
+(function () {
+    angular
+        .module('app')
+        .controller('DataCenterController', [ '$rootScope','$scope','NgMap','$http',
+            DataCenterController
+        ]);
 
-  angular
-    .module('app')
-    .controller('ProfileController', [
-      ProfileController
-    ]);
+    function DataCenterController($rootScope,$scope, NgMap,$http) {
 
-  function ProfileController() {
-    var vm = this;
+        var vm = this;
+        vm.isShowMap = true;
+        vm.isShowGrid = false;
 
-    vm.user = {
-      title: 'Admin',
-      email: 'contact@flatlogic.com',
-      firstName: '',
-      lastName: '' ,
-      company: 'FlatLogic Inc.' ,
-      address: 'Fabritsiusa str, 4' ,
-      city: 'Minsk' ,
-      state: '' ,
-      biography: 'We are young and ambitious full service design and technology company. ' +
-      'Our focus is JavaScript development and User Interface design.',
-      postalCode : '220007'
-    };
-  }
+        NgMap.getMap().then(function(map) {
+            console.log('map', map);
+            vm.map = map;
+        });
+
+        vm.clicked = function() {
+            alert('add code to redirect to data center detailed view');
+        };
+
+        vm.loadData = function (){
+            $http({
+                method: 'GET',
+                url: '/app/dc_api_mock.json'
+            }).then(function successCallback(response) {
+                // this callback will be called asynchronously
+                console.log('!!! using mocked api data. change the url to live api.')
+                vm.dcList = response.data;
+                vm.dc = vm.dcList[0];
+                // when the response is available
+            }, function errorCallback(response) {
+                // called asynchronously if an error occurs
+                // or server returns response with an error status.
+                console.log('unable to load data center data.')
+            });
+        }
+
+
+        vm.showMapView = function(){
+            vm.isShowMap = true;
+            vm.isShowGrid = false;
+            vm.loadData();
+
+        }
+
+        vm.showGridView = function(){
+            vm.isShowMap = false;
+            vm.isShowGrid = true;
+            vm.loadData();
+
+        }
+        vm.dcList = [];
+
+        vm.showDetail = function(e, dc) {
+            vm.dc = dc;
+            vm.map.showInfoWindow('foo-iw', dc.id);
+        };
+
+        vm.hideDetail = function() {
+            vm.map.hideInfoWindow('foo-iw');
+        };
+
+        vm.loadData();
+    }
 
 })();
+
